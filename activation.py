c93d33f9729975103bf452296e248f4f4c351310
Alex Wang <alexander.gj.wang@gmail.com>
2018-04-01 16:12:10 -0400

    added some activation functions

---
+++ activation.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def sigmoid(Z):
+    A = 1 / (1 + np.exp(-Z))
+
+    assert(A.shape == Z.shape)
+    return A
+
+def relu(Z):
+    A = np.maximum(0, Z)
+
+    assert(A.shape == Z.shape)
+    return A
+
+
+def tanh(Z):
+    A = (np.exp(Z) - np.exp(-Z)) / (np.exp(Z) + np.exp(-Z))
+
+    assert(A.shape == Z.shape)
+    return A
