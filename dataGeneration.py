a96f353cb3abafacc28ef4054608e670ea4fbd5a
Alex Wang <alexander.gj.wang@gmail.com>
2018-04-01 16:05:10 -0400

    make two interleaving half circles for data generation

---
+++ dataGeneration.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.datasets
+
+# Generate a dataset and plot it
+np.random.seed(0)
+X, y = sklearn.datasets.make_moons(200, noise=0.20)
+plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
+plt.savefig("data.png")
